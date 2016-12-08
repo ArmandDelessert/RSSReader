986a8543f7d032a78c04d679fdf0a8e01d0b4a2d
darkfull1 <edward.dessingy@gmail.com>
2016-12-08 13:25:22 +0100

    Article layout complet, article activity view set, article class creation

--- app/src/main/java/hes_so/rssreader/FeedViewActivity.java
+++ app/src/main/java/hes_so/rssreader/FeedViewActivity.java
@@ -1,8 +1,11 @@
 package hes_so.rssreader;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -10,15 +13,26 @@ import android.widget.Toast;
  */
 
 public class FeedViewActivity extends AppCompatActivity {
+    private TextView goToArticleActivityTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_view);
 
+        goToArticleActivityTextView = (TextView) findViewById(R.id.goToArticleActivityTextView);
+
         // Ajout du bouton retour dans la barre de menu
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        goToArticleActivityTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ArticleViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
